6df2fdcb17f0988b5870cc78730eb95bce08998e
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-10-16 13:14:09 +0300

    Starting implement the charts logic

--- src/components/index.js
+++ src/components/index.js
@@ -6,9 +6,13 @@ export { default as SmallSidebar } from "./SmallSidebar";
 export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as NavLinks } from "./NavLinks";
 export { default as FormRowSelect } from "./FormRowSelect";
-
 export { default as SearchContainer } from "./SearchContainer";
 export { default as JobsContainer } from "./JobsContainer";
 export { default as Job } from "./Job";
 export { default as Loading } from "./Loading";
 export { default as JobInfo } from "./JobInfo";
+export { default as StatsContainer } from "./StatsContainer";
+export { default as ChartsContainer } from "./ChartsContainer";
+export { default as StatItem } from "./StatItem";
+export { default as AreaChart } from "./AreaChart";
+export { default as BarChart } from "./BarChart";
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,25 +1,25 @@
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+import { useEffect } from "react";
+
 const Stats = () => {
-  // return (
-  //   <div className="form-row">
-  //     <label htmlFor="status" className="form-label">
-  //       status
-  //     </label>
-  //     <select
-  //       name="status"
-  //       value={status}
-  //       onChange={handleJobInput}
-  //       className="form-select"
-  //     >
-  //       {statusOptions.map((itemValue, index) => {
-  //         return (
-  //           <option key={index} value={itemValue}>
-  //             {itemValue}
-  //           </option>
-  //         );
-  //       })}
-  //     </select>
-  //   </div>
-  // );
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+  if (isLoading) {
+    return <Loading center />;
+  }
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
